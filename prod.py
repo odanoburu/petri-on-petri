678468889a6769d87b0854312478c23c9e621c37
bruno cuconato (@odanoburu) <bcclaro@gmail.com>
2022-10-24 16:12:51 -0300

    Working with development version of snakes

--- prod.py
+++ prod.py
@@ -26,8 +26,7 @@ prod.add_output('p2', 't2', Value(1))
 # t3
 prod.add_transition(Transition('t3'))
 prod.add_input('p2', 't3', Value(1))
-# this throws an error:
-#prod.add_input('p4', 't3', Inhibitor(Variable('y')))
+prod.add_input('p4', 't3', Inhibitor(Variable('y')))
 prod.add_output('p1', 't3', Value(1))
 
 # t4
@@ -43,29 +42,40 @@ prod.add_transition(Transition('tsink'))
 prod.add_input('p4', 'tsink', Value(1))
 prod.add_output('sink', 'tsink', Value(1))
 
-def step():
-    prod.transition('t1').fire(Substitution())
-    prod.transition('t1').fire(Substitution())
-    prod.transition('t1').fire(Substitution())
-    print("After thrice t1:", prod.get_marking())
+def nop_(*args, **kwargs):
+    return
 
+debug_ = nop_ # print
+
+def step():
+    debug_("Transition t1", prod.transition('t1').modes())
+    prod.transition('t1').fire(Substitution())
+    prod.transition('t1').fire(Substitution())
+    prod.transition('t1').fire(Substitution())
+    debug_("After thrice t1:", prod.get_marking())
+
+    debug_("Transition t2", prod.transition('t2').modes())
     prod.transition('t2').fire(Substitution())
-    print("After t2:", prod.get_marking())
+    debug_("After t2:", prod.get_marking())
 
     # show possible substitutions enabling the transition
-    print("Transition", prod.transition('t4').modes())
+    debug_("Transition t4", prod.transition('t4').modes())
     # choose the substitution we will use to enable the transition
     ## NOTE: in this case there's only one option of substitution, but
     ## we may want to choose when there are several
-    prod.transition('t4').fire(Substitution(x=0))
-    print("After t4:", prod.get_marking())
+    prod.transition('t4').fire(Substitution())
+    debug_("After t4:", prod.get_marking())
 
-    prod.transition('t3').fire(Substitution())
-    print("After t3:", prod.get_marking())
-
+    debug_("Transition tsink", prod.transition('tsink').modes())
     prod.transition('tsink').fire(Substitution())
-    print("After tsink:", prod.get_marking())
+    debug_("After tsink:", prod.get_marking())
+
+    debug_("Transition t3", prod.transition('t3').modes())
+    prod.transition('t3').fire(Substitution())
+    debug_("After t3:", prod.get_marking())
+
 
 prod.draw("prod-net-before.png")
 step()
 step()
+prod.draw("prod-net-after.png")
